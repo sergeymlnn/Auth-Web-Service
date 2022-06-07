18c8b47efea68835a88953b4610f48508e347818
sergeymlnn <malxao666@gmail.com>
2022-06-07 23:17:00 +0300

    Added new settings for JWT

--- app/settings.py
+++ app/settings.py
@@ -4,6 +4,18 @@ from pydantic import BaseSettings, Field
 class Settings(BaseSettings):
   """Defines settings of the application, using ENV-variables or default values"""
   api_uri: str = Field("http://127.0.0.1:8000", env="API_URI")
+  jwt_schema: str = Field("Bearer", env="JWT_SCHEMA")
+  jwt_signing_algorithm: str = Field(
+    default="HS256",
+    env="JWT_SIGNING_ALGORITHM",
+    description="JWT signing algorithm"
+  )
+  jwt_token_expiration_period: int = Field(
+    default=1,
+    env="JWT_TOKEN_EXPIRATION_PERIOD",
+    description="Minutes"
+  )
+  jwt_secret_key: str = Field(..., env="JWT_SECRET_KEY")
 
 
 settings = Settings()
